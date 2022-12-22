3e91d81d4f22ad2682ec27eca58bec99bde4d066
Aasjiel <skullmage.pvp@gmail.com>
2022-12-22 16:53:44 +0100

    barebones base project

---
+++ src/main/java/ch/bbw/es/m151springjpa/SpringJPACrudRepoPlayer.java
@@ -0,0 +1,13 @@
+package ch.bbw.es.m151springjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJPACrudRepoPlayer {
+
+   public static void main(String[] args) {
+      SpringApplication.run(SpringJPACrudRepoPlayer.class, args);
+   }
+
+}
---
+++ src/test/java/ch/bbw/es/m151springjpa/SpringJPACrudRepoWorldTransportTest.java
@@ -0,0 +1,13 @@
+package ch.bbw.es.m151springjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJPACrudRepoWorldTransportTest {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
