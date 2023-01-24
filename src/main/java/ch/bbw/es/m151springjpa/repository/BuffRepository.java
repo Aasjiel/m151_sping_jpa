567a8e2fc8e5b5a7c9a594abe0eb0007a5d5f4e6
Aasjiel <skullmage.pvp@gmail.com>
2023-01-24 20:08:02 +0100

    added Buff Repository

---
+++ src/main/java/ch/bbw/es/m151springjpa/repository/BuffRepository.java
@@ -0,0 +1,8 @@
+package ch.bbw.es.m151springjpa.repository;
+
+import ch.bbw.es.m151springjpa.model.Buff;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BuffRepository extends CrudRepository<Buff, Integer> {
+    
+}
