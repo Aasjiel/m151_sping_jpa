974c012f109dc5031bbce1ed3e1569e2c8703414
Aasjiel <skullmage.pvp@gmail.com>
2023-01-24 21:56:39 +0100

    current wip for buff relation

--- src/main/java/ch/bbw/es/m151springjpa/model/Buff.java
+++ src/main/java/ch/bbw/es/m151springjpa/model/Buff.java
@@ -1,16 +1,36 @@
 package ch.bbw.es.m151springjpa.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
+@Table(name = "buff")
+@NamedQuery(name = "Buff.findAll", query = "FROM Buff")
 public class Buff {
     @Id
+    @Column(name = "id", unique = true)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     int buffID;
 
+    @Column(name = "buff_name")
     String buff_name;
 
+    @Column(name = "buff_level")
     int buff_level;
+
+    @ManyToMany(mappedBy = "buffs")
+    @JsonIgnore
+    List<Player> players;
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
 
     public int getBuffID() {
         return buffID;
