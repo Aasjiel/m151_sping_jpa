c8010fa34d12180be459da353f82ef3d9ec0946d
Aasjiel <skullmage.pvp@gmail.com>
2023-01-25 20:44:40 +0100

    removed relation from Friend model due to not continuing it

--- src/main/java/ch/bbw/es/m151springjpa/model/Friends.java
+++ src/main/java/ch/bbw/es/m151springjpa/model/Friends.java
@@ -3,19 +3,15 @@ package ch.bbw.es.m151springjpa.model;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "friedns")
+@NamedQuery(name = "Friends.findAll", query = "FROM Friends")
 public class Friends {
     @Id
+    @Column(name = "id", unique = true)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     int friendshipID;
-
+    @Column(name = "friendship_rep")
     int friedShipRep;
-
-    @ManyToOne
-    @JoinColumn(name = "player1")
-    Player player1;
-
-    @ManyToOne
-    @JoinColumn(name = "player2")
-    Player player2;
 
 
     public int getFriendshipID() {
@@ -33,20 +29,5 @@ public class Friends {
     public void setFriedShipRep(int friedShipRep) {
         this.friedShipRep = friedShipRep;
     }
-
-    public Player getPlayer1() {
-        return player1;
-    }
-
-    public void setPlayer1(Player player1) {
-        this.player1 = player1;
-    }
-
-    public Player getPlayer2() {
-        return player2;
-    }
-
-    public void setPlayer2(Player player2) {
-        this.player2 = player2;
-    }
+    
 }
